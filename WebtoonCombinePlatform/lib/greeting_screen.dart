50478c3287cb9a7750419d7fe7d8068b6f146f0d
LimHyeonGyun <200hk@naver.com>
2025-03-10 18:51:02 +0900

    페이지 추가
    greetings 페이지를 작성할 dart 스크립트 추가

---
+++ WebtoonCombinePlatform/lib/greeting_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GreetingScreen extends StatelessWidget{
+  const GreetingScreen({super.key});
+
+  Widget build(BuildContext context){
+    return MaterialApp.router(
+
+    );
+  }
+}
